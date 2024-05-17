e75f0aeacca28f8925ce6624f56f6fe303ac0e84
PiPo <pipo.khomgrid@gmail.com>
2024-05-17 12:01:31 +0700

    ex1-2 javascript-callback-higher-order

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,25 @@
 // Exercise #1: For Each Function
 
-function forEach(array, operation) {
+function forEach(array, operation, number) {
   // Start coding here
+  // console.log(array);
+  for (let each of array) {
+    // console.log(each);
+    let newNumber = operation(each, number);
+    newEmployeeSalaries.push(newNumber);
+  }
+  return newEmployeeSalaries;
 }
+
+let Salary = function (num1, num2) {
+  // console.log(number);
+  let number = num1 + num2;
+  return number;
+};
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
-
+forEach(employeeSalaries, Salary, 5000);
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,7 +2,23 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let result = operation(array);
+  console.log(result);
+  if (result >= 5) {
+    console.log(`PASS ✅`);
+  } else {
+    console.log(`FAIL ❌`);
+  }
 }
+
+let checkPoint = function (array) {
+  let x = 0;
+  for (let each of array)
+    if (each > 70) {
+      x += 1;
+    }
+  return x;
+};
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
@@ -10,6 +26,6 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkPoint);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkPoint);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkPoint);
