8d5d4ef894b4533c2ee9a7e25546d28caa56fd82
Mon <tracer.coolkid@gmail.com>
2024-05-17 14:25:09 +0700

    feat:added callback  ex1-2 js

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,18 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let i = 0; i < array.length; i++){
+    operation(array[i]);
+    console.log(newEmployeeSalaries);
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
-// Using `forEach` function here
 
+// Using `forEach` function here
+forEach(employeeSalaries, function(salary){
+  newEmployeeSalaries.push(salary + 5000);
+});
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
