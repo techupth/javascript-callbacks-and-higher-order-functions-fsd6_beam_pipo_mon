6bed754b26ccd5a8974577480344e2764aedce8b
Jakkaphob <jkpbeam@gmail.com>
2024-05-17 14:00:18 +0700

    feat: add javascrpit callbacks ex 1-2 ad feature/beam

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,15 @@
-// Exercise #1: For Each Function
-
-function forEach(array, operation) {
-  // Start coding here
-}
-
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
+function forEach(array, operation) {
+  for (let i in array) {
+    operation(array[i]);
+    console.log(employeeSalaries);
+  }
+}
 // Using `forEach` function here
+forEach(employeeSalaries, function (salary) {
+  newEmployeeSalaries.push(salary + 5000);
+});
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,21 @@
 //Exercise #2: At Least Five Function
-
 function atLeastFive(array, operation) {
-  // Start coding here
+  let check = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 70) {
+      check++;
+    }
+  }
+  console.log(array);
+  return operation(check);
+}
+
+function checkIfPass(check) {
+  if (check >= 5) {
+    return "ผ่านเกณฑ์ ✅";
+  } else {
+    return "ไม่ผ่านเกณฑ์ ❌";
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +24,9 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkIfPass);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkIfPass);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkIfPass);
+console.log("Room 1 Result: " + scoreRoom1Result);
+console.log("Room 2 Result: " + scoreRoom2Result);
+console.log("Room 3 Result: " + scoreRoom3Result);
